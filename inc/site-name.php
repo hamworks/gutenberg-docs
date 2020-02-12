f6ee13b6a1c7dbb320704e0819441d8e7ec99574
Chiaki Obara <obara@ham.works>
2020-02-12 17:49:36 +0900

    ヘッダースタイル調整

--- inc/site-name.php
+++ inc/site-name.php
@@ -14,6 +14,6 @@ function the_site_name() {
 	if ( has_custom_logo() ) {
 		the_custom_logo();
 	} else {
-		echo wp_kses_post( '<a href="' . home_url( '/' ) . '"><span>' . get_bloginfo( 'name' ) . '</span></a>' );
+		echo '<a href="' . esc_attr( home_url( '/' ) ) . '"><span>' . esc_html( get_bloginfo( 'name' ) ) . '</span></a>';
 	}
 }
