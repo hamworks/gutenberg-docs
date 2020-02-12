1f1c4e3c16c1c33c5eb4eace8a1b364bc2ca024d
Chiaki Obara <obara@ham.works>
2020-02-12 17:25:27 +0900

    フロントページ作成

--- front-page.php
+++ front-page.php
@@ -12,12 +12,23 @@
  */
 
 get_header();
+?>
+<div class="site-main-visual"><img src="<?php header_image(); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" alt=""></div>
 
-if ( have_posts() ) :
-	while ( have_posts() ) :
-		the_post();
-		the_content();
-	endwhile;
-endif;
+<div class="site-container">
+	<div class="site-container__inner">
+		<main id="primary" class="site-main">
+			<?php
+			if ( have_posts() ) :
+				while ( have_posts() ) :
+					the_post();
+					the_content();
+				endwhile;
+			endif;
+			?>
+		</main>
+	</div><!-- /.site-container__inner -->
+</div><!-- /.site-container -->
 
+<?php
 get_footer();
