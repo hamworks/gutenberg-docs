4128f25c21bc817c95c493d0bf26fbf5776956e2
Chiaki Obara <obara@ham.works>
2020-02-13 18:43:25 +0900

    404ページ作成

--- 404.php
+++ 404.php
@@ -9,50 +9,27 @@
 
 get_header(); ?>
 
-	<main id="primary" class="site-main">
+<main id="primary">
+	<?php
+	if ( ! is_front_page() ) :
+		get_template_part( 'template-parts/breadcrumb' );
+	endif;
+	?>
+	<div class="site-container">
+		<header class="entry-header">
+			<div class="entry-header__inner">
+				<h1 class="entry-title">Error</h1>
+			</div><!-- /.entry-header__inner -->
+		</header><!-- /.entry-header -->
+		<div class="site-container__inner">
+			<div class="site-main">
+				<div class="entry-content">
+					<p>記事がみつかりませんでした。</p>
+				</div><!-- /.entry-content -->
+			</div><!-- /.site-main -->
+		</div><!-- /.site-container__inner -->
+	</div><!-- /.site-container -->
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'hamworks' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'hamworks' ); ?></p>
-
-				<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-				?>
-
-				<div class="widget widget_categories">
-					<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'hamworks' ); ?></h2>
-					<ul>
-					<?php
-						wp_list_categories( array(
-							'orderby'    => 'count',
-							'order'      => 'DESC',
-							'show_count' => 1,
-							'title_li'   => '',
-							'number'     => 10,
-						) );
-					?>
-					</ul>
-				</div><!-- .widget -->
-
-				<?php
-
-					/* translators: %1$s: smiley */
-					$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'hamworks' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-				?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #primary -->
-
+</main>
 <?php
 get_footer();
