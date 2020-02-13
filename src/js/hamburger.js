35c4c680142923deccb5af90d2b4aff7af443195
Chiaki Obara <obara@ham.works>
2020-02-13 20:49:42 +0900

    不要js削除

--- src/js/hamburger.js
+++
@@ -1,9 +0,0 @@
-/**
- * ハンバーガーメニューを設定するJS
- */
-import Menu from './Menu';
-import $ from 'jquery';
-
-$( function( ) {
-	new Menu( 'common-navigation' );
-} );
--- src/js/index.js
+++
@@ -1,1 +0,0 @@
-import './hamworks.js';
