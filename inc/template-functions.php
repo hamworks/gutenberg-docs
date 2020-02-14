9b9a531f384fe22431079c63fda90e6ba1d070fc
Chiaki Obara <obara@ham.works>
2020-02-14 17:01:11 +0900

    関数接頭辞修正

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -11,7 +11,7 @@
  * @param array $classes Classes for the body element.
  * @return array
  */
-function hamworks_body_classes( $classes ) {
+function hamdocs_body_classes( $classes ) {
 	// Adds a class of hfeed to non-singular pages.
 	if ( ! is_singular() ) {
 		$classes[] = 'hfeed';
@@ -19,14 +19,14 @@ function hamworks_body_classes( $classes ) {
 
 	return $classes;
 }
-add_filter( 'body_class', 'hamworks_body_classes' );
+add_filter( 'body_class', 'hamdocs_body_classes' );
 
 /**
  * Add a pingback url auto-discovery header for singularly identifiable articles.
  */
-function hamworks_pingback_header() {
+function hamdocs_pingback_header() {
 	if ( is_singular() && pings_open() ) {
 		echo '<link rel="pingback" href="', esc_url( get_bloginfo( 'pingback_url' ) ), '">';
 	}
 }
-add_action( 'wp_head', 'hamworks_pingback_header' );
+add_action( 'wp_head', 'hamdocs_pingback_header' );
