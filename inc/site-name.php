1f83bbbebec65b87f6ca5a70571456d58ef20475
Chiaki Obara <obara@ham.works>
2020-02-18 16:00:26 +0900

    ロゴをアイコン＋テキストに変更

--- inc/site-name.php
+++ inc/site-name.php
@@ -5,7 +5,6 @@
 function hamdocs_the_site_name() {
 	if ( has_custom_logo() ) {
 		the_custom_logo();
-	} else {
-		echo '<a href="' . esc_attr( home_url( '/' ) ) . '"><span>' . esc_html( get_bloginfo( 'name' ) ) . '</span></a>';
 	}
+	echo '<a href="' . esc_attr( home_url( '/' ) ) . '"><span>' . esc_html( get_bloginfo( 'name' ) ) . '</span></a>';
 }
