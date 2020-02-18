14e965f9384ceb4c528136862ac8b28ce3d714e5
Chiaki Obara <obara@ham.works>
2020-02-18 16:05:58 +0900

    グループブロックに幅狭用追加

--- src/js/add-advanced-style.js
+++ src/js/add-advanced-style.js
@@ -6,6 +6,14 @@ wp.blocks.registerBlockStyle( 'core/group', {
 	label: '大見出し'
 } );
 
+wp.blocks.registerBlockStyle( 'core/group', {
+	name: 'short',
+	label: '幅が狭いコンテンツ'
+} );
+
+/**
+ * Advanced Posts Blocks
+ **/
 wp.blocks.registerBlockStyle( 'advanced-posts-blocks/posts', {
 	name: 'thumbnail',
 	label: 'サムネイルつき'
